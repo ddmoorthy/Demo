f28ce2dda5bd4f167e022a34eb42501fa3ff6589
ddmoorthy <dakshina.deenadayalan@sunoida.com>
2022-05-02 18:18:34 +0530

    test commit

--- demo/src/main/java/com/vision/demo/controller/DemoController.java
+++ demo/src/main/java/com/vision/demo/controller/DemoController.java
@@ -1,16 +1,23 @@
 package com.vision.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.vision.demo.service.DemoService;
 
 @RestControllerAdvice
 @RequestMapping(path="controller")
 public class DemoController {
 
+	@Autowired
+	DemoService demoService;
+	
 	@RequestMapping(path="/method", method = RequestMethod.GET)
 	public String call() {
-		return "call";
+		int returnStatus = demoService.serviceCall();
+		return "call-"+returnStatus;
 	}
 	
 }
---
+++ demo/src/main/java/com/vision/demo/service/DemoService.java
@@ -0,0 +1,12 @@
+package com.vision.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+	
+	public int serviceCall() {
+		return 0;
+	}
+
+}
